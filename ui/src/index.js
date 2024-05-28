860f1db8ed93c952b08e528b9cd6dd74aafa1201
Debasis Mahanta <134546756+debasismahanta@users.noreply.github.com>
2024-05-28 13:09:27 +0530

    Update index.js

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './ui/src/index.css';
 import App from './ui/src/App';
 import * as serviceWorker from './serviceWorker';
 
